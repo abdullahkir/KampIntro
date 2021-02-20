ecca4ac569344753bbb432d8f9507c2503c07fac
Abdullah KIR <abdullah.406@hotmail.com>
2021-02-20 17:22:08 +0300

    mydictionaryproject

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> kitaplar = new MyDictionary<string>();
+            kitaplar.Add("Sis ve Gece");
+            kitaplar.Add("Patasana");
+            kitaplar.Add("Bab-ı Esrar");
+        }
+    }
+}
